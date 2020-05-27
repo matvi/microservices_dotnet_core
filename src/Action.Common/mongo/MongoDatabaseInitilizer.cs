bf0febf95c0ca2e7f36ae5f26868f62288dbd942
David Mata <dmata@OneIncSystems.com>
2020-05-26 20:55:30 -0500

    PROC-8 Fixing bugs

--- src/Action.Common/mongo/MongoDatabaseInitilizer.cs
+++ src/Action.Common/mongo/MongoDatabaseInitilizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Conventions;
@@ -14,20 +15,24 @@ namespace Action.Common.mongo
         private readonly bool _seed;
         private readonly IMongoDatabase _database;
         private readonly IDatabaseSeeder _databaseSeeder;
+        private readonly ILogger<MongoDatabaseInitilizer> _logger;
 
-        public MongoDatabaseInitilizer(IMongoDatabase database, IOptions<MongoOptions> options, IDatabaseSeeder databaseSeeder)
+        public MongoDatabaseInitilizer(IMongoDatabase database, IOptions<MongoOptions> options, IDatabaseSeeder databaseSeeder,
+        ILogger<MongoDatabaseInitilizer> logger)
         {
             _databaseSeeder = databaseSeeder;
+            _logger = logger;
             _database = database;
             _seed = options.Value.Seed;
         }
         public async Task InitializeAsync()
         {
+            _logger.LogInformation($"***2.0 InitilizedAsync _initialized : {_initialized}");
             if (_initialized) return;
 
             RegisterConventions();
             _initialized = true;
-
+            _logger.LogInformation($"***2.0 InitilizedAsync _seed : {_seed}");
             if (!_seed) return;
 
             await _databaseSeeder.SeedAsync();
--- src/Action.Services.Activities/Services/CustomMongoSeeder.cs
+++ src/Action.Services.Activities/Services/CustomMongoSeeder.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Action.Common.mongo;
 using Action.Services.Activities.Domain.Models;
 using Action.Services.Activities.Domain.Repositories;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace Action.Services.Activities.Services
@@ -11,15 +13,19 @@ namespace Action.Services.Activities.Services
     public class CustomMongoSeeder : MongoSeeder
     {
         private readonly ICategoryRepository _categoryRepository;
+        private readonly ILogger<CustomMongoSeeder> _logger;
 
         public CustomMongoSeeder(IMongoDatabase database, 
-            ICategoryRepository categoryRepository) : base(database)
+            ICategoryRepository categoryRepository,
+            ILogger<CustomMongoSeeder> logger) : base(database)
         {
             _categoryRepository = categoryRepository;
+            _logger = logger;
         }
 
         protected override async Task CustomSeedAsync()
         {
+            _logger.LogInformation("***4Sedding categories");
             var categories = new List<string>
             {
                 "work",
